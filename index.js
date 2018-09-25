13c80d560e02767e2f3cf1ef8284259a1990ce4b
Keyvan Mir Mohammad Sadeghi <keyvan.m.sadeghi@gmail.com>
2018-09-25 16:46:28 +0330

    Add linting

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 class AsyncCounter {
-    constructor(countTimes) {
-      let currentCount = 0;
-      this.countTimes = countTimes;
-      this.ready = new Promise(resolveReady => {
-        this.finished = new Promise(resolveFinished => {
-          const count = () => {
-            currentCount++;
-            if (currentCount >= this.countTimes) {
-              resolveFinished();
-            }
-            return currentCount;
-          };
-          this.count = () => this.ready.then(() => count());
-          resolveReady();
-        });
+  constructor(countTimes) {
+    let currentCount = 0;
+    this.countTimes = countTimes;
+    this.ready = new Promise(resolveReady => {
+      this.finished = new Promise(resolveFinished => {
+        const count = () => {
+          currentCount++;
+          if (currentCount >= this.countTimes) {
+            resolveFinished();
+          }
+          return currentCount;
+        };
+        this.count = () => this.ready.then(() => count());
+        resolveReady();
       });
-    }
+    });
   }
+}
 
-  module.exports = {
-      AsyncCounter,
-      asyncCounter: (countTimes) => new AsyncCounter(countTimes)
-  };
+module.exports = {
+  AsyncCounter,
+  asyncCounter: countTimes => new AsyncCounter(countTimes)
+};
--- test.js
+++ test.js
@@ -1,17 +1,17 @@
 import test from 'ava';
-import { asyncCounter } from '.';
+import {asyncCounter} from '.';
 
-test('e2e', async t => {
-    let countTimes = 3;
-    const counter = asyncCounter(countTimes);
-    const awaitFinish = async () => {
-        await counter.finished;
-        t.pass();
-    };
+test('e2e', t => {
+  let countTimes = 3;
+  const counter = asyncCounter(countTimes);
+  const awaitFinish = async () => {
+    await counter.finished;
+    t.pass();
+  };
 
-    for (; countTimes > 0; countTimes--) {
-        setTimeout(() => counter.count().then(count => console.log(`${count} of ${counter.countTimes} times`)), countTimes * 500);
-    }
+  for (; countTimes > 0; countTimes--) {
+    setTimeout(() => counter.count().then(count => console.log(`${count} of ${counter.countTimes} times`)), countTimes * 500);
+  }
 
-    return awaitFinish();
+  return awaitFinish();
 });

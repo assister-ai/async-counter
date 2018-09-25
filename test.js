2df0b02c8285a48ea5759687c761e59e57c6959f
Keyvan Mir Mohammad Sadeghi <keyvan.m.sadeghi@gmail.com>
2018-09-25 16:26:11 +0330

    Update test

--- test.js
+++ test.js
@@ -4,9 +4,14 @@ import { asyncCounter } from '.';
 test('e2e', async t => {
     let countTimes = 3;
     const counter = asyncCounter(countTimes);
+    const awaitFinish = async () => {
+        await counter.finished;
+        t.pass();
+    };
+
     for (; countTimes > 0; countTimes--) {
-        setTimeout(() => counter.count().then(count => console.log(`${count} of ${counter.countTimes} times`), countTimes * 500));
+        setTimeout(() => counter.count().then(count => console.log(`${count} of ${counter.countTimes} times`)), countTimes * 500);
     }
-    await counter.finished;
-    t.pass();
+
+    return awaitFinish();
 });
